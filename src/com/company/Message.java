57ce80c448265320e4b0cfcf1f8d95f4fb5d5d04
Dr. Auxin <paul.torrillo@gmail.com>
2017-01-30 10:31:42 -0500

    Initial commit

---
+++ src/com/company/Message.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by pt696 on 1/30/17.
+ */
+public class Message {
+    public Message (String number, String text, String time)
+    {
+        phone = number;
+        md = text;
+        clock = time;
+    }
+    public String phone;
+    public String md;
+    public String clock;
+
+
+}
